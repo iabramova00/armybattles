e7d9373ce4f34a48a23457bdf1679fabb74dc26d
iabramova00 <167477279+iabramova00@users.noreply.github.com>
2025-07-14 19:19:24 +0300

    Added Unit Tests for Warriors and Armies

--- src/main/kotlin/Warrior.kt
+++ src/main/kotlin/Warrior.kt
@@ -3,90 +3,154 @@ package softserve.academy
 open class Warrior(
     var health: Int = 50,
     val attack: Int = 5
+) {
+    val isAlive: Boolean
+        get() = health > 0
+}
+
+class Knight : Warrior(
+    attack = 7
 )
 
-val Warrior.isAlive: Boolean
-    get() = health > 0
-
-infix fun Warrior.hits(other: Warrior) {
-    other.health -= this.attack
+fun fight(warrior1: Warrior, warrior2: Warrior): Boolean {
+    while (warrior1.isAlive && warrior2.isAlive) {
+        warrior2.health -= warrior1.attack
+        if (warrior2.isAlive) {
+            warrior1.health -= warrior2.attack
+        }
+    }
+    return warrior1.isAlive
 }
-
-class Knight : Warrior(attack = 7)
 
 class Army {
-    private val troops = mutableListOf<Warrior>()
+    private val units = mutableListOf<Warrior>()
 
-    fun addUnit(warrior: Warrior) {
-        troops.add(warrior)
-    }
+    val hasUnits: Boolean
+        get() = units.isNotEmpty()
 
-    val isAlive: Boolean
-        get() = troops.any { it.isAlive }
-
-    val champion: Warrior
-        get() = troops.find { it.isAlive }!!
-
-    fun iterator(): Iterator<Warrior> = troops.iterator()
-}
-
-fun Army.addUnits(n: Int, factory: () -> Warrior) {
-    repeat(n) { addUnit(factory()) }
-}
-
-fun fight(first: Warrior, second: Warrior): Boolean {
-    while (first.isAlive && second.isAlive) {
-        first hits second
-        if (second.isAlive) {
-            second hits first
+    fun addUnits(factory: () -> Warrior, count: Int) {
+        repeat(count) {
+            units.add(factory())
         }
     }
-    return first.isAlive
+
+    fun nextFighter(): Warrior? = units.firstOrNull()
+
+    fun removeFighter() {
+        if (units.isNotEmpty()) {
+            units.removeAt(0)
+        }
+    }
 }
 
-//fun fight(first: Army, second: Army): Boolean {
-//    while (first.isAlive and second.isAlive) {
-//        fight(first.champion, second.champion)
-//    }
-//    return first.isAlive
-//}
-fun fight(first: Army, second: Army): Boolean {
-    val firstIterator = first.iterator()
-    if (!firstIterator.hasNext()) return false
+fun fight(army1: Army, army2: Army): Boolean {
+    while (army1.hasUnits && army2.hasUnits) {
+        val fighter1 = army1.nextFighter()!!
+        val fighter2 = army2.nextFighter()!!
 
-    val secondIterator = second.iterator()
-    if (!secondIterator.hasNext()) return true
-
-    var firstChampion = firstIterator.next()
-    var secondChampion = secondIterator.next()
-
-    while (true) {
-        val res = fight(firstChampion, secondChampion)
-        if (res) {
-            if (!secondIterator.hasNext()) { return true }
-            secondChampion = secondIterator.next()
+        if (fight(fighter1, fighter2)) {
+            army2.removeFighter()
         } else {
-            if (!firstIterator.hasNext()) { return false }
-            firstChampion = firstIterator.next()
+            army1.removeFighter()
         }
     }
+    return army1.hasUnits
 }
 
-fun main() {
-    val myArmy = Army()
-    myArmy.addUnits(3) { Knight() }
 
-    val enemyArmy = Army()
-    enemyArmy.addUnits(3) { Warrior() }
+//open class Warrior(
+//var health: Int = 50,
+//val attack: Int = 5
+//)
+//
+//val Warrior.isAlive: Boolean
+//get() = health > 0
+//
+//infix fun Warrior.hits(other: Warrior) {
+//other.health -= this.attack
+//}
+//
+//class Knight : Warrior(attack = 7)
+//
+//class Army {
+//private val troops = mutableListOf<Warrior>()
+//
+//fun addUnit(warrior: Warrior) {
+//troops.add(warrior)
+//}
+//
+//val isAlive: Boolean
+//get() = troops.any { it.isAlive }
+//
+//val champion: Warrior
+//get() = troops.find { it.isAlive }!!
+//
+//fun iterator(): Iterator<Warrior> = troops.iterator()
+//}
+//
+//fun Army.addUnits(n: Int, factory: () -> Warrior) {
+//repeat(n) { addUnit(factory()) }
+//}
+//
+//fun fight(first: Warrior, second: Warrior): Boolean {
+//while (first.isAlive && second.isAlive) {
+//first hits second
+//if (second.isAlive) {
+//second hits first
+//}
+//}
+//return first.isAlive
+//}
+//
+////fun fight(first: Army, second: Army): Boolean {
+////    while (first.isAlive and second.isAlive) {
+////        fight(first.champion, second.champion)
+////    }
+////    return first.isAlive
+////}
+//fun fight(first: Army, second: Army): Boolean {
+//val firstIterator = first.iterator()
+//if (!firstIterator.hasNext()) return false
+//
+//val secondIterator = second.iterator()
+//if (!secondIterator.hasNext()) return true
+//
+//var firstChampion = firstIterator.next()
+//var secondChampion = secondIterator.next()
+//
+//while (true) {
+//val res = fight(firstChampion, secondChampion)
+//if (res) {
+//if (!secondIterator.hasNext()) {
+//return true
+//}
+//secondChampion = secondIterator.next()
+//} else {
+//if (!firstIterator.hasNext()) {
+//return false
+//}
+//firstChampion = firstIterator.next()
+//}
+//}
+//}
+//
+//fun main() {
+//val myArmy = Army()
+//myArmy.addUnits(3) { Knight() }
+//
+//val enemyArmy = Army()
+//enemyArmy.addUnits(3) { Warrior() }
+//
+//val army3 = Army()
+//army3.addUnits(20) { Warrior() }
+//army3.addUnits(5) { Knight() }
+//
+//val army4 = Army()
+//army4.addUnits(30) { Warrior() }
+//
+//check(fight(myArmy, enemyArmy) == true)
+//check(fight(army3, army4) == false)
+//println("OK")
+//}
+//*/
 
-    val army3 = Army()
-    army3.addUnits(20) { Warrior() }
-    army3.addUnits(5) { Knight() }
-
-    val army4 = Army()
-    army4.addUnits(30) { Warrior() }
-
-    check(fight(myArmy, enemyArmy) == true)
-    check(fight(army3, army4) == false)
-    println("OK")
-}
--- src/test/kotlin/FightTest.kt
+++ src/test/kotlin/FightTest.kt
@@ -4,41 +4,106 @@ import org.junit.jupiter.api.Test
 import softserve.academy.Knight
 import softserve.academy.Warrior
 import softserve.academy.fight
-import softserve.academy.isAlive
 
 class FightTest {
-    @Test
-    fun `Warrior should win over Warrior`() {
-        val chuck = Warrior()
-        val bruce = Warrior()
-
-        val res = fight(chuck, bruce)
-
-        assertTrue(res) { "Warrior should win Warrior" }
-        assertTrue(chuck.isAlive) { "Warrior winner should be alive" }
-        assertFalse(bruce.isAlive) { "Warrior defeated should not be alive" }
-    }
 
     @Test
-    fun `Warrior should loose to Knight`() {
-        val dave = Warrior()
-        val carl = Knight()
-
-        val res = fight(dave, carl)
-
-        assertFalse(res) { "Warrior should lose to Knight" }
-        assertTrue(carl.isAlive) { "Knight winner should be alive" }
-        assertFalse(dave.isAlive) { "Warrior defeated should not be alive" }
-    }
-
-    @Test
-    @DisplayName("1. Fight")
-    fun `Warrior loses when fights against Knight`() {
+    @DisplayName("1. Warrior loses when fighting a Knight")
+    fun `warrior should lose against a knight`() {
+        // given
         val carl = Warrior()
-        val jon = Knight()
+        val jim = Knight()
 
-        val res = fight(carl, jon)
+        // when
+        val result = fight(carl, jim)
 
-        assertFalse(res) { "Warrior should lose to Knight" }
+        // then
+        assertEquals(false, result, "A Warrior should not win against a Knight.")
+    }
+
+    @Test
+    @DisplayName("2. Knight wins when fighting a Warrior")
+    fun `knight should win against a warrior`() {
+        // given
+        val ramon = Knight()
+        val slevin = Warrior()
+
+        // when
+        val result = fight(ramon, slevin)
+
+        // then
+        assertEquals(true, result, "A Knight should win against a Warrior.")
+    }
+
+    @Test
+    @DisplayName("3. First of two Warriors wins the fight")
+    fun `first warrior should be alive after fighting another warrior`() {
+        // given
+        val bob = Warrior()
+        val mars = Warrior()
+
+        // when
+        fight(bob, mars)
+
+        // then
+        assertEquals(true, bob.isAlive, "The first warrior to attack should survive a duel with another warrior.")
+    }
+
+    @Test
+    @DisplayName("4. Knight survives a fight with a Warrior")
+    fun `knight should be alive after fighting a warrior`() {
+        // given
+        val zeus = Knight()
+        val godkiller = Warrior()
+
+        // when
+        fight(zeus, godkiller)
+
+        // then
+        assertEquals(true, zeus.isAlive, "A Knight should survive a duel with a Warrior.")
+    }
+
+    @Test
+    @DisplayName("5. Second of two Warriors loses the fight")
+    fun `second warrior should not be alive after fighting another warrior`() {
+        // given
+        val husband = Warrior()
+        val wife = Warrior()
+
+        // when
+        fight(husband, wife)
+
+        // then
+        assertEquals(false, wife.isAlive, "The second warrior should not survive a duel with another warrior.")
+    }
+
+    @Test
+    @DisplayName("6. Knight survives being attacked by a Warrior")
+    fun `knight should still be alive after being attacked by a warrior`() {
+        // given
+        val dragon = Warrior() // The attacker
+        val knight = Knight()  // The defender
+
+        // when
+        fight(dragon, knight)
+
+        // then
+        assertEquals(true, knight.isAlive, "The knight should survive the duel even when attacked first.")
+    }
+
+    @Test
+    @DisplayName("7. Wounded Knight loses to a fresh Warrior")
+    fun `a wounded knight should lose to a fresh warrior`() {
+        // given
+        val unit1 = Warrior()
+        val unit2 = Knight() // This knight will get wounded
+        val unit3 = Warrior() // This warrior is fresh
+
+        // when
+        fight(unit1, unit2)
+        val result = fight(unit2, unit3)
+
+        // then
+        assertEquals(false, result, "The wounded knight should lose the second fight.")
     }
 }
